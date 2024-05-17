cee44a6271b58a524a46ff19d7e68ebd8d4e75f0
Ekaterina Ambartsumova <ekaterina.amba@mail.ru>
2024-05-17 20:34:42 +0300

    ADD: models.py

--- askme_ambartsumova/app/models.py
+++ askme_ambartsumova/app/models.py
@@ -1,3 +1,98 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+'''
+Table Question {
+  id int [PK]
+  title varchar
+  text_body varchar
+  answer_id int
+  like_id int
+}
+
+
+Table Answer {
+  id int [PK]
+  text_body varchar
+  like_id int
+}
+
+Table Tag {
+  id int [PK]
+  name varchar
+}
+
+
+
+Table Question_Tag {
+  tag_id int
+  question_id int
+ 
+  
+}
+
+
+
+Table Profile {
+  id int [PK] 
+  avatar image
+  
+}
+
+Table User {
+  id int [PK]
+  login varchar
+  email varchar
+  password varchar
+  profile_id int [ref: - Profile.id]
+
+  question_id int
+  answer_id int
+}
+
+
+
+Table QuestionLike {
+  id int [PK]
+  value int
+  user_id int [ref: - User.id]
+}
+
+Table AnswerLike {
+  id int [PK]
+  value int
+  user_id int [ref: - User.id]
+}
+
+
+'''
+
+class Profile(models.Model):
+    avatar = models.ImageField(null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class AnswerLike(models.Model):
+    value = models.IntegerField()
+    user = models.OneToOneField(Profile, on_delete=models.PROTECT)
+
+class Answer(models.Model):
+    text_body = models.CharField(max_length=1000)
+    like = models.ForeignKey(AnswerLike, on_delete=models.CASCADE)
+
+class QuestionLike(models.Model):
+    value = models.IntegerField()
+    user = models.OneToOneField(Profile, on_delete=models.PROTECT)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=15)
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text_body = models.CharField(max_length=1000)
+    answer = models.ForeignKey(Answer, on_delete = models.CASCADE)
+    like = models.ForeignKey(QuestionLike, on_delete = models.PROTECT)
+
+    tags = models.ManyToManyField(Tag, blank=True)
